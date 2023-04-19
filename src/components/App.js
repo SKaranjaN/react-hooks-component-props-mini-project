be2a70e7f5ad503467f421894d7bc909d5673b24
Stephen <skaranja654@gmail.com>
2023-04-19 17:37:15 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,55 +1,16 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from "./Header";
+import ArticleList from "./ArticleList";
+import About from "./About";
 
 console.log(blogData);
-
-function Header(props) {
-  return (
-    <header>
-      <h1>{props.blogName}</h1>
-      <About about={props.about} />
-    </header>
-  );
-}
-
-function About(props) {
-  const src = props.src ? props.src : "https://via.placeholder.com/215";
-  const alt = props.alt ? props.alt : "blog logo";
-
-  return (
-    <aside>
-      <img src={src} alt={alt} />
-      <p>{props.about}</p>
-    </aside>
-  );
-}
-
-function ArticleList(props) {
-  const { posts } = props;
-  return (
-    <main>
-      {posts.map((post) => (
-        <Article key={post.id} post={post} />
-      ))}
-    </main>
-  );
-}
-
-function Article(props) {
-  const { title, date = "January 1, 1970", preview } = props.post;
-  return (
-    <article>
-      <h3>{title}</h3>
-      <small>{date}</small>
-      <p>{preview}</p>
-    </article>
-  );
-}
 
 function App() {
   return (
     <div className="App">
-      <Header blogName={blogData.name} about={blogData.about} />
+      <Header blogName={blogData.name} />
+      <About image={blogData.image} about={blogData.about} />
       <ArticleList posts={blogData.posts} />
     </div>
   );
